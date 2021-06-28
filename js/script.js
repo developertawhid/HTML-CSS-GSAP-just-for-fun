a24ba7a3879ca68e730dfddb9b793b165d8a240e
SM Tawhid <86618204+developertawhid@users.noreply.github.com>
2021-06-29 01:20:06 +0600

    Mouse move and start GSAP effect
    Mouse move and start GSAP effect.
    Use: GSAP.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// google search
+/*==============greensock tweenmax==============*/
+
+
+document.body.addEventListener("mousemove", e => {
+    gsap.to(".circle", {
+        x : e.clientX,
+        y : e.clientY,
+        stagger: -0.5,
+    });
+});
+
+
